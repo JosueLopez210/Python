d03ba9a761a65d2e62d6b6d9faaa4f64b125c3f4
Josue Lopez <jos21lopeze@gmail.com>
2025-02-12 19:50:56 -0600

    FizzBuzz

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print ("Hello World in Python")
-print ("Añadiendo una Linea al Codigo")
+
+for x in range (1,100):
+    if (x%3==0)and x%5==0:
+        print ("FizzBuzz")
+    else:
+        if x%3==0:
+            print ("Fizz")
+        elif x%5==0:
+            print ("Buzz")
+        else:
+            print(x)
