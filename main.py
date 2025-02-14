cabd876eed9ca193323e10c14dcaaab0c2903027
Josue Lopez <jos21lopeze@gmail.com>
2025-02-14 15:25:04 -0600

    Corrigiendo codigo a PEP8

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+
+
 def fizzBuzz():
     for x in range (1,100):
-        if (x%3==0)and x%5==0:
+        if x % 3 == 0 and x % 5 == 0:
             print ("FizzBuzz")
         else:
-            if x%3==0:
+            if x % 3 == 0 :
                 print ("Fizz")
-            elif x%5==0:\
+            elif x % 5 == 0 :
                 print ("Buzz")
             else:
                 print(x)
